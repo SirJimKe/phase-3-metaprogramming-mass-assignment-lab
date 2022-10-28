3731b4cd72d39009ac55c5ebd87741768d70ff29
SirJimKe <jimmymutuku1@gmail.com>
2022-10-28 04:16:18 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+
+binding.pry
